bf9a990ba345c10ac99d1aeaba4577af6792a78d
Sergio Cañete <sergio13896@gmail.com>
2020-07-27 17:02:05 +0200

    Update debloat.bat

--- debloat.bat
+++ debloat.bat
@@ -3,18 +3,14 @@ adb shell pm disable-user --user 0 com.facebook.appmanager
 adb shell pm disable-user --user 0 com.facebook.services
 adb shell pm disable-user --user 0 com.facebook.system
 adb shell pm disable-user --user 0 com.google.android.apps.tachyon
-adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
 adb shell pm disable-user --user 0 com.google.android.music
-adb shell pm uninstall -k --user 0 com.google.android.music
 adb shell pm disable-user --user 0 com.google.android.videos
-adb shell pm uninstall -k --user 0 com.google.android.videos
 adb shell pm disable-user --user 0 com.miui.analytics
 adb shell pm disable-user --user 0 com.miui.bugreport
 adb shell pm disable-user --user 0 com.miui.notes
 adb shell pm disable-user --user 0 com.miui.player
 adb shell pm disable-user --user 0 com.miui.videoplayer
 adb shell pm disable-user --user 0 com.xiaomi.mipicks
-adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
 adb shell pm disable-user --user 0 com.xiaomi.glgm
 adb shell pm disable-user --user 0 com.miui.hybrid
 adb shell pm disable-user --user 0 com.xiaomi.payment
@@ -28,23 +24,20 @@ adb shell pm disable-user --user 0 com.baidu.input_mi
 adb shell pm disable-user --user 0 com.baidu.duersdk.opensdk
 adb shell pm disable-user --user 0 com.mipay.wallet.in
 adb shell pm disable-user --user 0 com.xiaomi.midrop
-adb shell pm uninstall -k --user 0 com.xiaomi.midrop
 adb shell pm disable-user --user 0 com.miui.compass
 adb shell pm disable-user --user 0 com.xiaomi.scanner
 adb shell pm disable-user --user 0 com.android.calendar
 adb shell pm disable-user --user 0 com.miui.gallery
-adb shell pm uninstall -k --user 0 com.miui.gallery
 adb shell pm disable-user --user 0 com.mi.android.globalminusscreen
 adb shell pm disable-user --user 0 com.facemoji.lite.xiaomi
 adb shell pm disable-user --user 0 com.miui.miservice
 adb shell pm disable-user --user 0 com.google.android.marvin.talkback
-adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
 adb shell pm disable-user --user 0 com.android.printspooler
 adb shell pm disable-user --user 0 com.android.bips
 adb shell pm disable-user --user 0 com.android.wallpaper.livepicker
 adb shell pm disable-user --user 0 com.miui.videoplayer
 adb shell pm disable-user --user 0 com.google.ar.lens
-#adb shell pm disable-user --user 0 com.miui.home
+adb shell pm disable-user --user 0 com.miui.home
 adb shell pm disable-user --user 0 com.miui.fmservice
 adb shell pm disable-user --user 0 com.caf.fmradio
 adb shell pm disable-user --user 0 com.miui.cloudservice
@@ -79,9 +72,16 @@ adb shell pm disable-user --user 0 com.mi.webkit.core
 adb shell pm disable-user --user 0 com.miui.daemon
 adb shell pm disable-user --user 0 com.miui.fm
 adb shell pm disable-user --user 0 com.miui.translation.kingsoft
-adb shell pm disable-user --user 0 com.xiaomi.finddevice
 adb shell pm disable-user --user 0 com.miui.backup
-
+adb shell pm disable-user --user 0 com.xiaomi.finddevice
+#
+adb shell pm uninstall -k --user 0 com.xiaomi.finddevice
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.google.android.music
+adb shell pm uninstall -k --user 0 com.google.android.videos
+adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
+adb shell pm uninstall -k --user 0 com.miui.gallery
+adb shell pm uninstall -k --user 0 com.xiaomi.midrop
 
 
 pause
